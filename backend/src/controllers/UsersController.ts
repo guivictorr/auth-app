7edd55d52e014306b67143205c2790c365c7255b
Guilherme <guilhermevictor20@gmail.com>
2020-11-05 17:49:52 -0300

    user repository

---
+++ backend/src/controllers/UsersController.ts
@@ -0,0 +1,7 @@
+// import { Response, Request } from 'express';
+
+// class UsersController {
+//   public async index(request: Request, response: Response): Promise<Response> {}
+
+//   public async create(request: Request, response: Response): Promise<Response> {}
+// }
