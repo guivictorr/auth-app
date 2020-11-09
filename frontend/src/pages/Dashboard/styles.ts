c161f83d3fcee5e649df80cce17c0bcc108c3644
Guilherme <guilhermevictor20@gmail.com>
2020-11-09 14:20:19 -0300

    sign out button style

--- frontend/src/pages/Dashboard/styles.ts
+++ frontend/src/pages/Dashboard/styles.ts
@@ -8,6 +8,107 @@ export const Container = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: column;
+`;
+
+export const Content = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    align-self: flex-start;
+    border: none;
+    border-radius: 12px;
+    margin-bottom: 20px;
+    padding: 6px;
+    background-color: transparent;
+    color: #2f80ed;
+    font-size: 16px;
+    font-weight: bold;
+    cursor: pointer;
+    transition: all 0.2s ease;
+
+    &:hover {
+      margin-left: 5px;
+      svg {
+        opacity: 1;
+        transform: translateX(0);
+      }
+    }
+
+    svg {
+      transition: all 0.2s ease;
+      opacity: 0;
+      transform: translateX(5px);
+    }
+  }
+
+  ul {
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    flex-direction: column;
+    border: 1px solid #bdbdbd;
+    border-radius: 24px;
+    width: 845px;
+    list-style: none;
+
+    header {
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 30px 50px;
+
+      div {
+        p {
+          color: #e0e0e0;
+          font-size: 24px;
+        }
+
+        span {
+          font-size: 13px;
+          color: #828282;
+        }
+      }
+
+      button {
+        border: 1px solid #bdbdbd;
+        border-radius: 12px;
+        width: 95px;
+        height: 38px;
+        background-color: transparent;
+        color: #bdbdbd;
+        font-size: 16px;
+        font-weight: bold;
+        cursor: pointer;
+      }
+    }
+
+    li {
+      padding: 30px 50px;
+      display: flex;
+      align-items: center;
+      border-top: 1px solid #bdbdbd;
+      width: 100%;
+
+      span {
+        width: 280px;
+        color: #828282;
+        font-size: 14px;
+      }
+
+      p {
+        color: #e0e0e0;
+        font-size: 18px;
+        font-weight: 500;
+      }
+    }
+  }
 
   footer {
     display: flex;
@@ -18,68 +119,5 @@ export const Container = styled.div`
     margin-top: 5px;
     color: #bdbdbd;
     font-size: 12px;
-  }
-`;
-
-export const Content = styled.ul`
-  display: flex;
-  align-items: flex-start;
-  justify-content: center;
-  flex-direction: column;
-  border: 1px solid #bdbdbd;
-  border-radius: 24px;
-  width: 845px;
-  list-style: none;
-
-  header {
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 30px 50px;
-
-    div {
-      p {
-        color: #e0e0e0;
-        font-size: 24px;
-      }
-
-      span {
-        font-size: 13px;
-        color: #828282;
-      }
-    }
-
-    button {
-      border: 1px solid #bdbdbd;
-      border-radius: 12px;
-      width: 95px;
-      height: 38px;
-      background-color: transparent;
-      color: #bdbdbd;
-      font-size: 16px;
-      font-weight: bold;
-      cursor: pointer;
-    }
-  }
-
-  li {
-    padding: 30px 50px;
-    display: flex;
-    align-items: center;
-    border-top: 1px solid #bdbdbd;
-    width: 100%;
-
-    span {
-      width: 280px;
-      color: #828282;
-      font-size: 14px;
-    }
-
-    p {
-      color: #e0e0e0;
-      font-size: 18px;
-      font-weight: 500;
-    }
   }
 `;
